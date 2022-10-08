f861bbb4994f54f5a22c58fbadcc5609ebe26c27
Giacomo Alessandroni <g.alessandroni@gmail.com>
2022-10-08 12:42:42 +0200

    Create tavola-pitagorica.cpp

---
+++ tavola-pitagorica.cpp
@@ -0,0 +1,51 @@
+// tavola-pitagorica.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
+//
+
+#include <iostream>
+// Colonne e righe della tavola pitagorica
+int c = 12, r = 12;
+
+// Numero di spazi da inserire tra un numero e il successivo
+void spazi(int n) {
+	int s = 3;
+	if (n > 9)
+		s = 2;
+	if (n > 99)
+		s = 1;
+	for (int k = 0; k < s; k++)
+		std::cout << " ";
+}
+
+// Formattazione delle righe e colonne
+void linea() {
+	std::cout << " ";
+	// Inserisce la formattazione per ogni colonna
+	for (int j = 1; j <= c; j++)
+		std::cout << "+-----";
+	// Chiude la colonna e va a capo
+	std::cout << "+\n";
+}
+
+void main() {
+	// Controllo righe
+	for (int i = 1; i <= r; i++)
+	{
+		// Inizio tabella e riga
+		linea();
+		// Controllo colonne
+		for (int j = 1; j <= c; j++)
+		{
+			// Inserisce l'inizio cella
+			std::cout << " |";
+			// Inserisce gli spazi per un corretto allineamento a destra
+			spazi(i*j);
+			// Inserisce il numero della tavola pitagorica
+			std::cout << i * j;
+		}
+		// Inserisce la fine cella e passa alla riga successiva
+		std::cout << " |\n";
+	}
+	// Fine tabella
+	linea();
+	system("pause");
+}
