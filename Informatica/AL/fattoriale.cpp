553b2812744697216c03675d9d1b60a046142f25
Giacomo Alessandroni <g.alessandroni@gmail.com>
2022-10-11 19:51:50 +0200

    Update fattoriale.cpp

--- Informatica/AL/fattoriale.cpp
+++ Informatica/AL/fattoriale.cpp
@@ -1,20 +1,34 @@
+/*********************************************************************************************************************
+ * fattoriale.cpp                                                                                                    *
+ * Calcolo del fattoriale di un numero mediante algoritmo ricorsivo                                                  *
+ *                                                                                                                   *
+ * Funzioni ricorsive                                                                                                *
+ *********************************************************************************************************************/
+
+#include "stdafx.h"
 #include <iostream>
 
-using namespace std;
-
+// Algoritmo per il calcolo del fattoriale di un numero intero
 int fattoriale(int k) {
+  // Se il numero in input è superiore a zero
   if (k > 0) {
+    // Il fattoriale è il numero dato moltiplicato per fattoriale del numero precedente
     return k * fattoriale(k - 1);
   } else {
+    // La procedura continua ricorsivamente fino al fattoriale(0) = 1 per definizione
     return 1;
   }
 }
 
-int main() {
-    int n;
-    cout << "Inserire il numero di cui calcolare il fattoriale: ";
-    cin >> n;
+// Programma principale
+void main() {
+  // Acquisisce il numero di cui calcolare il fattoriale  
+  int n;
+  std::cout << "Inserire il numero di cui calcolare il fattoriale: ";
+  std::cin >> n;
+  
+  // Calcola il fattoriale del numero dato ed espone il risultato
   int result = fattoriale(n);
-  cout << result;
-  return 0;
+  std::cout << result;
+  system("pause");
 }
