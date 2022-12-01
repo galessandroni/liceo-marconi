7a76b899ce69f32eeacba861e62bd911178ad79e
Giacomo Alessandroni <g.alessandroni@gmail.com>
2022-12-01 17:58:03 +0100

    Create qrySchedina.sql

---
+++ Informatica/DB/qrySchedina.sql
@@ -0,0 +1,3 @@
+SELECT Giornata, SquadraCasa, SquadraFuoriCasa, PCasa, PFuoriCasa, IIf([PCasa]-[PFuoriCasa]>0,"1",IIf([PCasa]-[PFuoriCasa]=0,"X","2")) AS Schedina
+FROM tblPartite
+WHERE Giornata=4;
