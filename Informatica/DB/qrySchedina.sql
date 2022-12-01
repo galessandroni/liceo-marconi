1bf5f50bdf319bf60487466edc0172def032a13d
Giacomo Alessandroni <g.alessandroni@gmail.com>
2022-12-01 18:03:12 +0100

    Update qrySchedina.sql

--- Informatica/DB/qrySchedina.sql
+++ Informatica/DB/qrySchedina.sql
@@ -1,3 +1,3 @@
-SELECT Giornata, SquadraCasa, SquadraFuoriCasa, PCasa, PFuoriCasa, IIf([PCasa] - [PFuoriCasa] > 0, "1", IIf([PCasa] - [PFuoriCasa] = 0, "X", "2")) AS Schedina
+SELECT Giornata, SquadraCasa, SquadraFuoriCasa, PCasa, PFuoriCasa, IIf(PCasa > PFuoriCasa, "1", IIf(PCasa = PFuoriCasa, "X", "2")) AS Schedina
 FROM tblPartite
 WHERE Giornata = 4;
